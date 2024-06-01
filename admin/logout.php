48e353973696794be913a7683a7c8da338f10c4f
Tezza <144427359+TezzaPrayoga@users.noreply.github.com>
2024-06-01 16:18:34 +0700

    Delete admin directory

--- admin/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-echo "<script>
-alert('Anda telah logout');
-location='login.php';
-</script>";
--- admin/partials/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="sticky-footer ">
-    <div class="container my-auto ">
-        <div class="copyright text-center my-auto">
-            <span>Copyright © <a href=target="_blank">Roliev Delvio</a> <?php echo date('Y'); ?></span>
-        </div>
-    </div>
-</footer>
